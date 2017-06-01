94393333cc3b44668530d6499eee1086e5a744c3
honeyjonny <honeyjonny@gmail.com>
2017-06-01 22:19:13 +0400

    Core of template

---
+++ OwinOdataTest/ODataConfigurator.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.OData.Edm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.OData.Builder;
+
+namespace OwinOdataTest
+{
+    class ODataConfigurator
+    {
+        public static IEdmModel GetEdmModel()
+        {
+            ODataModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Product>("Products");
+
+            return builder.GetEdmModel();
+        }
+    }
+}
---
+++ OwinOdataTest/Startup.cs
@@ -0,0 +1,26 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.OData.Extensions;
+
+namespace OwinOdataTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+            config.MapODataServiceRoute(
+                routeName: "ODataRoute",
+                routePrefix: "api/odata",
+                model: ODataConfigurator.GetEdmModel());
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
